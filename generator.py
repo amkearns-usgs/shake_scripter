cc36d403d068138766663e77c7073f4bbf5f4f07
Aaron Kearns <akearns@usgs.gov>
2017-07-19 10:24:29 -0600

    Add step function, some comments

--- generator.py
+++ generator.py
@@ -3,6 +3,16 @@ import math, os.path as path, os
 
 tau = math.pi * 2
 
+# basically 3 main components to this code in a few variations
+# 1. generator functions for signals that send out a time series list as well as
+# the ms interval between samples (and a list of the time of each sample)
+# 2. output functions that convert those lists into csv files and provides 
+# helpful metadata to advise the process of converting that to shake table 
+# trajectories (sample rate, initial displacement, etc.)
+# 3. dispatcher code that provides a main routine that can be used to generate
+# the data files used by the shake tables, prompting for relevant parameters and
+# doing any necessary checks on the inputs provided by the user
+
 def sin_gen(amp, freq, mins):
     # sin wave formula: -A * cos(2*pi*f*t)
     # we use this formula because it means that at 0
@@ -62,6 +72,26 @@ def noise_gen(amp, mins):
     wave_series = [amp*i/s for i in noise]
     return (ms_interval, timestamps, wave_series)
 
+def step_gen(amp, mins):
+    sampling = 100.
+    step = 1 / sampling
+    ms_interval = step * 1000
+    time = 0
+    timestamps = []
+    wave_series = []
+    max_time = mins * 60
+    step_start = 10
+    step_end = max_time - 10
+    while time < max_time:
+        timestamps.append(time)
+        if time > step_start and time < step_end:
+            wave_series.append(amp)
+        else:
+            wave_series.append(0.)
+        time += step
+    return (ms_interval, timestamps, wave_series)    
+    
+
 def get_folder():
     home_folder = path.expanduser('~/Trajectories/')
     if not path.isdir(home_folder):
@@ -88,7 +118,6 @@ def collect_data(times, wave):
     line += ", "
     line += str(time_now)
     csv_data += line
-
     return csv_data
 
 def sweep_out(amp, freq0, freq1, mins):
@@ -99,7 +128,7 @@ def sweep_out(amp, freq0, freq1, mins):
     file_prefix += str(int(freq0))
     file_prefix += "hz_to_"
     file_prefix += str(int(freq1))
-    file_prefix += "_hz"
+    file_prefix += "hz_"
     file_prefix += str(int(amp))
     file_prefix += "amp_"
     file_prefix += str(int(mins))
@@ -107,9 +136,7 @@ def sweep_out(amp, freq0, freq1, mins):
     csv_name = file_prefix + ".csv"
     # used to inform of any additional data worth noting
     metadata_name = file_prefix+".metadata.txt"
-    metadata_name = file_prefix+".metadata.txt"
-    metadata = ""
-    metadata += "Make sure input and output intervals"
+    metadata = "Make sure input and output intervals"
     metadata += " are set to " + str(ms_itv) + " ms.\n"
     metadata += "The sweep wave should be able to be started"
     metadata += " from the home position.\n"
@@ -136,15 +163,12 @@ def noise_out(amp, mins):
     csv_name = file_prefix + ".csv"
     # used to inform of any additional data worth noting
     metadata_name = file_prefix+".metadata.txt"
-    metadata_name = file_prefix+".metadata.txt"
-    metadata = ""
-    metadata += "Make sure input and output intervals"
+    metadata = "Make sure input and output intervals"
     metadata += " are set to " + str(ms_itv) + " ms.\n"
     metadata += "You may need to adjust the starting position of the table.\n"
     metadata += "It is advised to set the table starting position to "
     metadata += str(amp)+" mm.\n"
     metadata += "(The shake table only allows positive-value positions.)\n"
-    metadata += "The value of t to is " + str(amp) + "\n"
     metadata += "The timeseries is the CSV's first column.\n"
     metadata += "Don't forget to select the proper axis!"
     print metadata
@@ -163,7 +187,7 @@ def sin_out(amp, freq, mins):
     file_prefix = home_folder
     file_prefix += "sin_"
     file_prefix += str(int(freq))
-    file_prefix += "_hz"
+    file_prefix += "hz_"
     file_prefix += str(int(amp))
     file_prefix += "amp_"
     file_prefix += str(int(mins))
@@ -171,10 +195,36 @@ def sin_out(amp, freq, mins):
     csv_name = file_prefix + ".csv"
     # used to inform of any additional data worth noting
     metadata_name = file_prefix+".metadata.txt"
-    metadata = ""
-    metadata += "Make sure input and output intervals"
+    metadata = "Make sure input and output intervals"
     metadata += " are set to " + str(ms_itv) + " ms.\n"
     metadata += "The sin wave should be able to be started"
+    metadata += " from the home position.\n"
+    metadata += "The timeseries is the CSV's first column.\n"
+    metadata += "Don't forget to select the proper axis!"
+    print metadata
+    # collect the csv data
+    csv_data = collect_data(times, wave)
+    with open(csv_name, 'w') as csv_file:
+        csv_file.write(csv_data)
+    with open(metadata_name, 'w') as meta_file:
+        meta_file.write(metadata)
+    return
+
+def step_out(amp, mins):
+    (ms_itv, times, wave) = step_gen(amp, mins)
+    home_folder = get_folder()
+    file_prefix = home_folder
+    file_prefix += "step_"
+    file_prefix += str(int(amp))
+    file_prefix += "amp_"
+    file_prefix += str(int(mins))
+    file_prefix += "mins"
+    csv_name = file_prefix + ".csv"
+    # used to inform of any additional data worth noting
+    metadata_name = file_prefix+".metadata.txt"
+    metadata = "Make sure input and output intervals"
+    metadata += " are set to " + str(ms_itv) + " ms.\n"
+    metadata += "The step function should be able to be started"
     metadata += " from the home position.\n"
     metadata += "The timeseries is the CSV's first column.\n"
     metadata += "Don't forget to select the proper axis!"
@@ -200,10 +250,11 @@ def prompt_numeric(msg, data_fmt):
     
 
 def usr_prompts():
-    msg = "Generate sine, sine chirp (sweep), or white noise "
+    msg = "Generate sine, sine chirp (sweep), pulse (step), or white noise "
     msg += "pattern. "
     print msg
-    valid = ['s','c','w']
+    char = ''
+    valid = ['s','c','w','p']
     while char not in valid:
         if char != '':
             print "\nSorry, that input [", char, "] is invalid."
@@ -220,11 +271,11 @@ def usr_prompts():
         msg = "Please specify a frequency."
         data = "FREQ: "
         freq = prompt_numeric(msg, data)
-        msg = "Please specify a duration (minutes)"
+        msg = "Please specify a duration. (minutes)"
         data = "DUR: "
         mins = prompt_numeric(msg, data)
         sin_out(amp, freq, mins)
-    else if char == 'c':
+    elif char == 'c':
         print "Creating a swept sine wave."
         msg = "Please specify an amplitude."
         data = "AMP: "
@@ -235,11 +286,11 @@ def usr_prompts():
         msg = "Please specify a termination frequency."
         data = "END FREQ: "
         freq1 = prompt_numeric(msg, data)
-        msg = "Please specify a duration (minutes)"
+        msg = "Please specify a duration. (minutes)"
         data = "DUR: "
         mins = prompt_numeric(msg, data)
         sweep_out(amp, freq0, freq1, mins)
-    else if char == 'w':
+    elif char == 'w':
         print "Creating a white noise function."
         msg = "Please specify an amplitude (total range of noise function)."
         data = "AMP: "
@@ -248,15 +299,16 @@ def usr_prompts():
         data = "DUR: "
         mins = prompt_numeric(msg, data)
         noise_out(amp, mins)
-    """else if char == 'p':
+    elif char == 'p':
         print "Creating a step function."
         msg = "Please specify an amplitude (peak value of step)."
         data = "AMP: "
         amp = prompt_numeric(msg, data)
-        msg = "Please specify a duration (minutes)"
+        msg = "The step function has lead-in and out times of 10 seconds.\n"
+        msg += "Please specify the duration of the step (minutes)."
         data = "DUR: "
         mins = prompt_numeric(msg, data)
-        step_out(amp, mins)"""
+        step_out(amp, mins)
 
     print "Data has been saved in the folder labeled 'Trajectories',"
     print " under the current user's home directory."
